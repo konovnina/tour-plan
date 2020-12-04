1e81498bffaef52f15231e52ab88ddd9a35f5c59
konovnina <ea.knvn@gmail.com>
2020-12-05 00:06:56 +0400

    create: modal dialog

--- js/main.js
+++ js/main.js
@@ -1,65 +1,95 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+$(document).ready(function () {
+  $("#phone-footer").mask("+7 (999) 999-9999");
+  $("#phone-modal").mask("+7 (999) 999-9999");
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  //Keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-});
+    //Keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+  });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  //Keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-});
+    //Keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+  });
 
 
-ymaps.ready(init);
+  ymaps.ready(init);
 
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map-wrapper", {
-    center: [7.890703, 98.294772],
-    zoom: 15,
-  }),
-    myPlacemark = new ymaps.Placemark([7.890703, 98.294772], {
-      balloonContentHeader: "DoubleTree by Hilton Hotel",
-      balloonContentBody: "Pa Tong, Kathu, Phuket, Thailand",
-      balloonContentFooter: "+66 76 34 08 50",
-      hintContent: "Click here to read more"
-    });
-  myMap.geoObjects.add(myPlacemark);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map-wrapper", {
+      center: [7.890703, 98.294772],
+      zoom: 15,
+    }),
+      myPlacemark = new ymaps.Placemark([7.890703, 98.294772], {
+        balloonContentHeader: "DoubleTree by Hilton Hotel",
+        balloonContentBody: "Pa Tong, Kathu, Phuket, Thailand",
+        balloonContentFooter: "+66 76 34 08 50",
+        hintContent: "Click here to read more"
+      });
+    myMap.geoObjects.add(myPlacemark);
 
-}
+  }
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log('Click');
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle('navbar-bottom--visible');
+  var menuButton = $(".menu-button");
+  menuButton.on('click', function () {
+    $(".navbar-bottom").toggleClass('navbar-bottom--visible');
 
-});
+  });
+
+  document.body.addEventListener('keyup', function (event) {
+    var key = event.keyCode;
+    if (key == 27) {
+      console.log('close modal');
+      closeModal(event);
+    };
+  }, false);
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible')
+    modalDialog.addClass('modal__dialog--visible')
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible')
+    modalDialog.removeClass('modal__dialog--visible')
+  }
+}); 
